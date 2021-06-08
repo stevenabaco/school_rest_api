eb0a0dad2f420acb0f3d2046b5596fc0ac605082
Steven Abaco <stevenabaco@gmail.com>
2021-06-08 07:54:45 -0500

    Add bcryot module

--- routes.js
+++ routes.js
@@ -1,19 +1,30 @@
 'use strict';
+
 // Load Modules
 const express = require('express');
 
 // Import dependencies
 const { asyncHandler } = require('./middleware/async-handler');
 const { User, Course } = require('./models');
+const { authenticateUser } = require('./middleware/auth-user');
 
 // Construct a router instance
 const router = express.Router();
 
-// GET route that will return the currently authenticated
+// GET route that will return the currently AUTHENTICATED
 // user along with a 200 HTTP status code
 
-router.get('/users', asyncHandler(async (req, res) => {
+router.get(
+	'/users',
+	authenticateUser,
+	asyncHandler(async (req, res) => {
+		const user = req.currentUser;
+		res.json({
+			name: `${user.firstName} ${user.lastName}`,
+			username: user.emailAddress,
+		});
+	})
+);
 
-}))
 
-module.exports = router
+module.exports = router;
